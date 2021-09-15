4d81c91ce27c7e42c54bc4ca440c6879113d1d31
re1mond <re1mond@outlook.com>
2021-09-15 09:26:17 +0300

    express

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'tarasok2013',
-  database : 'printcase'
-});
+// var mysql      = require('mysql');
+// var connection = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : 'root',
+//   password : 'tarasok2013',
+//   database : 'printcase'
+// });
  
-connection.connect();
+// connection.connect();
  
-connection.query('SELECT * from themes', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results);
-});
+// connection.query('SELECT * from themes', function (error, results, fields) {
+//   if (error) throw error;
+//   console.log('The solution is: ', results);
+// });
  
-connection.end();
+// connection.end();
+
+const express = require('express')
+const app = express()
+const port = 3094
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
